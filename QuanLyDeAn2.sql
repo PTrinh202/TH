b788b14fb999bcae91023bd7f23c5f1ac4eceb34
PTrinh202 <118448037+PTrinh202@users.noreply.github.com>
2023-02-22 10:35:06 +0700

    Add files via upload

---
+++ QuanLyDeAn2.sql
@@ -0,0 +1,87 @@
+﻿USE QuanLyDeAn2
+create table DEAN(
+MaDA varchar(2) primary key,
+TenDA nvarchar(50) null,
+DiemDA varchar(20) null
+);
+
+create table PHANCONG(
+MaNV varchar(9) primary key,
+MaDA varchar(2) foreign key (MaDA) references DEAN(MaDA),
+ThoiGian numeric(18,0) null
+);
+
+create table NHANVIEN(
+MaNV varchar(9) primary key,
+HoNV nvarchar(15) null,
+TenLot nvarchar(30) null,
+TenNV nvarchar(30) null,
+NgSinh smalldatetime null,
+DiaChi nvarchar(150) null,
+Phai nvarchar(3) null,
+Luong numeric(18,0) null,
+Phong varchar(2)
+);
+
+create table PHONGBAN(
+MaPhg varchar(2) primary key,
+TenPhg nvarchar(20) null
+);
+
+create table THANNHAN(
+MaNV varchar(9) primary key,
+TenTN varchar(20) foreign key (MaNV) references NHANVIEN(MaNV),
+NgaySinh smalldatetime null,
+Phai varchar(3) null,
+QuanHe varchar(15) null
+);
+
+--Câu 4:
+alter table PHONGBAN alter column TenPhg nvarchar(30);
+
+alter table DEAN alter column DiemDA nvarchar(20);
+
+alter table THANNHAN alter column TenTN nvarchar(20);
+alter table THANNHAN alter column Phai nvarchar(3);
+alter table THANNHAN alter column QuanHe nvarchar(15);
+
+alter table NHANVIEN add SoDienThoai varchar(15);
+
+alter table NHANVIEN
+drop column SoDienThoai;
+
+--Câu 5:
+insert into NHANVIEN 
+(MaNV, HoNV, TenLot,TenNV, NgSinh, DiaChi,Phai,Luong,Phong)
+values (123, 'Đinh', 'Bá', 'Tiến', 1982-02-27, 'Mộ Đức', 'Nam', '', 4),
+	(234, 'Nguyễn', 'Thanh', 'Tùng', 1956-8-12, 'Sơn Tịnh','','Nam',5),
+	(345, 'Bùi', 'Thúy', 'Vũ', '', 'Tư Nghĩa','Nữ','',4),
+	(567, 'Nguyễn', 'Mạnh', 'Hùng', 1985-3-25, 'Sơn Tịnh', 'Nam', '', 5),
+	(678, 'Trần ', 'Hồng', 'Quang', '', 'Bình Sơn', 'Nam', '',5);
+
+insert into PHONGBAN
+(MaPhg,TenPhg)
+values
+(1,'Quản Lý'),
+(4,'Điều Hành'),
+(5,'Nghiên Cứu');
+
+insert into DEAN
+(MaDA,TenDA,DiemDA)
+values
+(1,'Nâng cao cắp muối ','Sa Huỳnh'),
+(10,'Xây dựng máu chế biến thủy sản ','Dung Quất'),
+(2,'Phát triển ha tầng mạng ','Quảng Ngãi'),
+(20,'Truyền tải cơ quan ','Quảng Ngãi'),
+(3,'Tin học hóa trường học ','BaTo');
+
+insert into PHANCONG
+(MaNV,MaDA,ThoiGian)
+values
+(123,1,33),
+(123,2,8),
+(345,10,10),
+(345,20,10),
+(456,1,20),
+(456,2,20);
+--Câu 6
---
+++ TH B2.sql
@@ -0,0 +1,96 @@
+﻿USE AdventureWorks2008R2
+--Câu 1
+select d.SalesOrderID, OrderDate, SubTotal = sum(OrderQty * UnitPrice)
+from sales.SalesOrderDetail d join Sales.SalesOrderHeader h on d.SalesOrderID = h.SalesOrderID
+where  MONTH(OrderDate) = 6 and YEAR(OrderDate) = 2008  
+group by d.SalesOrderID, OrderDate
+having SUM(OrderQty * UnitPrice) > 70000
+
+--Câu 2
+select t.TerritoryID, CountofCus= COUNT(c.CustomerID) , Subtotal=SUM(d.OrderQty * d.UnitPrice)  
+from Sales.SalesTerritory t join Sales.Customer  c on t.TerritoryID=c.TerritoryID
+							join Sales.SalesOrderHeader h on h.CustomerID=h.CustomerID
+							join Sales.SalesOrderDetail d on h.SalesOrderID=d.SalesOrderID
+where CountryRegionCode = 'US' 
+group by t.TerritoryID
+
+--Câu 3
+select SalesOrderID, CarrierTrackingNumber, Subtotal=SUM(OrderQty * UnitPrice) 
+from Sales.SalesOrderDetail
+group by SalesOrderID, CarrierTrackingNumber
+having CarrierTrackingNumber like '4BD%'
+
+--Câu 4
+select pro.ProductID, pro.Name, AverageofQty=AVG(det.OrderQty) 
+from Sales.SalesOrderDetail det join Production.Product pro on det.ProductID = pro.ProductID
+where det.UnitPrice < 25
+group by pro.ProductID, pro.Name
+having AVG(det.OrderQty) > 5
+
+--Câu 5
+select JobTitle, CountofPerson=count(BusinessEntityID) 
+from HumanResources.Employee 
+group by JobTitle
+having COUNT(BusinessEntityID) > 20
+
+--Câu 6
+select v.BusinessEntityID, v.Name, ProductID, sumofQty = SUM(OrderQty), SubTotal = SUM(OrderQty * UnitPrice)
+from Purchasing.Vendor v 
+	join Purchasing.PurchaseOrderHeader a on a.VendorID = v.BusinessEntityID
+	join Purchasing.PurchaseOrderDetail b on a.PurchaseOrderID = b.PurchaseOrderID
+where v.Name like '%Bicycles'
+group by v.BusinessEntityID, v.Name, ProductID
+having SUM(OrderQty * UnitPrice) > 800000
+
+--Câu 7
+select p.ProductID, p.Name, countofOrderID = COUNT(o.SalesOrderID), Subtotal = sum(OrderQty * UnitPrice) 
+from Production.Product p join Sales.SalesOrderDetail o on p.ProductID = o.ProductID
+						  join sales.SalesOrderHeader h on h.SalesOrderID = o.SalesOrderID
+where Datepart(q, OrderDate) = 1 and YEAR(OrderDate) = 2008
+group by p.ProductID, p.Name
+having sum(OrderQty * UnitPrice) > 10000 and COUNT(o.SalesOrderID) > 500
+
+--Câu 8
+select PersonID, FirstName +' '+ LastName as Fullname, CountOfOrders=count(SalesOrderID)
+from Person.Person p join Sales.Customer c on p.BusinessEntityID=c.CustomerID
+					join Sales.SalesOrderHeader h on h.CustomerID= c.CustomerID
+where YEAR(OrderDate) between 200 and 2008
+group by PersonID, FirstName +' '+ LastName
+having count(SalesOrderID)>25
+
+--Câu 9
+select p.ProductID, Name, CountofOrderQty=sum(OrderQty), yearofSale=year(OrderDate)
+from Production.Product p join Sales.SalesOrderDetail d on p.ProductID=d.ProductID
+						join Sales.SalesOrderHeader h on d.SalesOrderID=d.SalesOrderID
+where name like '%Bike' or name like '%Sport'
+group by p.ProductID, Name, year(OrderDate)
+having sum(OrderQty)>500
+
+--Câu 10
+select d.DepartmentID, d.Name, AvgofRate=avg(Rate)
+from HumanResources.Department d 
+	join HumanResources.EmployeeDepartmentHistory h on d.DepartmentID=h.DepartmentID
+	join HumanResources.EmployeePayHistory e on h.BusinessEntityID=e.BusinessEntityID
+group by d.DepartmentID, d.Name
+having avg(Rate)>30
+
+--II/ SUBQUERY
+--Câu 1: Liệt kê các sản phẩm gồm các thông tin product names và product ID có trên 100 đơn đặt hàng trong tháng 7 năm 2008
+select ProductID, Name
+from Production.Product
+where ProductID in (
+	select ProductID
+	from  Sales.SalesOrderDetail d join Sales.SalesOrderHeader h on d.SalesOrderID=h.SalesOrderID
+	where MONTH(OrderDate)=7 and YEAR(OrderDate)=2008
+	group by  ProductID
+	having COUNT(*)>100)
+--Câu 2: Liệt kê các sản phẩm (ProductID, name) có số hóa đơn đặt hàng nhiều nhất trong tháng 7/2008
+select p.ProductID, Name
+from Production.Product p join Sales.SalesOrderDetail d on p.ProductID=d.ProductID
+	                      join Sales.SalesOrderHeader h on d.SalesOrderID=h.SalesOrderID
+where  MONTH(OrderDate)=7 and YEAR(OrderDate)=2008
+group by p.ProductID, Name
+having COUNT(*)>=all( select COUNT(*)
+	from Sales.SalesOrderDetail d join Sales.SalesOrderHeader h on d.SalesOrderID=h.SalesOrderID
+	where MONTH(OrderDate)=7 and YEAR(OrderDate)=2008
+	group by ProductID)
